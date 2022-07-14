3b8fadb23278429e48824f4c968e3325bbd1b61f
nicoclau <legrandnico@yahoo.com>
2022-07-14 15:43:02 +0200

    First Version
    Set timezone within Linux Docker Container

---
+++ TimeZoneLinuxContainerDateTime/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.Json;
+
+namespace TimeZoneLinuxContainerDateTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);          
+        }
+    }
+}
